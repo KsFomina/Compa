a41081e0825e3cf8621fc4ec46b17cdb07be6804
sarkatvas <katyasarycheva03@gmail.com>
2024-09-23 17:12:38 +0300

    кнопка с дизайном

--- Compa/my-app/src/Items/Tabs.js
+++ Compa/my-app/src/Items/Tabs.js
@@ -34,7 +34,16 @@ const Tabs = () => {
                 {activeTab === 1 }
                 {activeTab === 0 && <EventList/>}
             </div>
-            <button onClick={handleCreateEvent}>Создать событие</button>
+            <div className='fixed-button-container' >
+            <button className='fixed-button' onClick={handleCreateEvent}>
+             <svg width="100">
+                <rect/>
+                <circle cx="49.5" cy="37.5" r="20.5" fill="white" />
+                <rect x="47.5413" y="25.5" width="2.49259" height="22.575" rx="1.2463" fill="#FF735C" stroke="#FF735C" />
+                <rect x="36.5" y="39.0338" width="2.49259" height="22.575" rx="1.2463" transform="rotate(-90 36.5 38.0338)" fill="#FF735C" stroke="#FF735C" />
+             </svg>            
+            </button>
+            </div>
         </div>
     );
 };
