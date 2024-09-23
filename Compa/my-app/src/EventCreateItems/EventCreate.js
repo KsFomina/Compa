3130f783294c6e3a7c9b6696767080a11681efcf
KsFomina <sloveckija@gmail.com>
2024-09-23 22:49:03 +0300

    немного стиля

--- Compa/my-app/src/EventCreateItems/EventCreate.js
+++ Compa/my-app/src/EventCreateItems/EventCreate.js
@@ -4,7 +4,7 @@ import './EventCreate.css';
 const EventCreate = () => {
     return (
         <div>
-            <button class='button' >
+            <button class='buttonClose'>
             <svg width="58" height="58" viewBox="0 0 58 58" fill="#FF735C" xmlns="http://www.w3.org/2000/svg">
             <rect x="19.8073" y="22.5698" width="2.49259" height="22.575" rx="1.2463" transform="rotate(-45 19.8073 22.5698)" fill="white" stroke="white" />
             <rect x="21.5698" y="38.5328" width="2.49259" height="22.575" rx="1.2463" transform="rotate(-135 21.5698 38.5328)" fill="white" stroke="white" />
@@ -13,7 +13,7 @@ const EventCreate = () => {
             <div class="container"> 
             <input class="input_mini" type='text' placeholder='Название события' ></input>
             <textarea class="input_maxi" type='text' placeholder='Описание событие'></textarea>
-            <p>Теги</p>
+            <p >Теги</p>
             </div>
         </div>
     );
