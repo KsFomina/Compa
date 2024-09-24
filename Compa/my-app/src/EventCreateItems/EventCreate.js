97d5fff9690e0c58ee60bcd7a520e27ef2de4895
KsFomina <sloveckija@gmail.com>
2024-09-24 22:44:54 +0300

    поля для дат

--- Compa/my-app/src/EventCreateItems/EventCreate.js
+++ Compa/my-app/src/EventCreateItems/EventCreate.js
@@ -11,9 +11,15 @@ const EventCreate = () => {
             </svg>
             </button>
             <div class="container"> 
-            <input class="input_mini" type='text' placeholder='Название события' ></input>
+            <input class="input_mini" type='text' placeholder='Название события' required></input>
             <textarea class="input_maxi" type='text' placeholder='Описание событие'></textarea>
             <p >Теги</p>
+            <button class='buttonTegs'>
+                <text class="textButton">выбрать</text></button>
+            <input class="input_mini" type='number' min="0" max="100" placeholder='Количество участников' required></input>
+            <p>Дата и время начала события</p>
+            <input class="input_calendar" type='date'></input>
+            <input class="input_mini"></input>
             </div>
         </div>
     );
