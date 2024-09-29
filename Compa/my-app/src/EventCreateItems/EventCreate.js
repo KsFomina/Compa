5460b1bd97c5da85800373221501c818bf792f3d
sarkatvas <katyasarycheva03@gmail.com>
2024-09-29 18:16:30 +0300

    кнопка закрытия окна поменьше

--- Compa/my-app/src/EventCreateItems/EventCreate.js
+++ Compa/my-app/src/EventCreateItems/EventCreate.js
@@ -11,9 +11,9 @@ const EventCreate = () => {
     return (
         <div>
             <button class='buttonClose' onClick={handleCreateEvent}>
-            <svg width="58" height="58" viewBox="0 0 58 58" fill="#FF735C" xmlns="http://www.w3.org/2000/svg">
-            <rect x="19.8073" y="22.5698" width="2.49259" height="22.575" rx="1.2463" transform="rotate(-45 19.8073 22.5698)" fill="white" stroke="white" />
-            <rect x="21.5698" y="38.5328" width="2.49259" height="22.575" rx="1.2463" transform="rotate(-135 21.5698 38.5328)" fill="white" stroke="white" />
+            <svg width="30" height="30" viewBox="0 0 58 58" fill="#FF735C" xmlns="http://www.w3.org/2000/svg">
+            <rect x="17" y="24" width="2.49259" height="22.575" rx="1.2463" transform="rotate(-45 19.8073 22.5698)" fill="white" stroke="white" />
+            <rect x="20" y="36" width="2.49259" height="22.575" rx="1.2463" transform="rotate(-135 21.5698 38.5328)" fill="white" stroke="white" />
             </svg>
             </button>
             <div class="container"> 
