4c35a72f29bb679f44a6ff768da7c5f553c58bdd
sarkatvas <katyasarycheva03@gmail.com>
2024-10-09 12:37:20 +0300

    страница авторизации

---
+++ Compa/my-app/src/Authorization/AutorizationPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const AutorizationPage = () => {
+return(
+    <div>
+        <p>Вход</p>
+        <img/>
+        <form className="Autorization-form">
+            <input type="text"/>
+            <input type="password"/>
+            <input type={"submit"}/>
+        </form>
+    </div>
+ 
+
+);
+}
+
+export default AutorizationPage;
