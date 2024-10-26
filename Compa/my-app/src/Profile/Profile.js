b5ad821b80de7b7eb2c1cda5d9595eabd471be5a
sarkatvas <katyasarycheva03@gmail.com>
2024-10-26 19:28:06 +0300

    профиль полусырой

--- Compa/my-app/src/Profile/Profile.js
+++ Compa/my-app/src/Profile/Profile.js
@@ -1,20 +1,38 @@
 import React from "react";
-import { Modal, Button } from "antd";
 import "./Profile.css";
+import PhotoGallery from "./PhotoGallery";
 
 const Profile = () => {
+  const data = ["tag1", "tag2", "tag3"]; // Данные для тегов
+
   return (
     <>
       <div className="container">
-        <img className="profile-image" />
-        <div>
-          <h2 className="h2-profile">user name</h2>
+        <div className="profile-container">
+          <img className="profile-image" />
+          <div>
+            <h2 className="h2-profile">user name</h2>
+            <div>
+              <div className="InfoAboutUser-profile">
+                information about user information about user information about
+                user information about user
+              </div>
+            </div>
+          </div>
 
-          <div>information about user</div>
+          <ul className="tegList-profile">
+            {data.map((item, index) => (
+              <li key={index}>
+                <div className="teg-profile">{item}</div>
+              </li>
+            ))}
+          </ul>
+          <div className="MY-event">Мои события</div>
+          <div>events events</div>
+          <div>
+            <PhotoGallery />
+          </div>
         </div>
-        <div>tegs</div>
-        <div>Мои события</div>
-        <div>photo</div>
       </div>
     </>
   );
