ba1c3616cc0ece0fb7404855ea665925e09c6828
sarkatvas <katyasarycheva03@gmail.com>
2024-10-27 17:10:38 +0300

    поправила и добавила, но еще не до конца

--- Compa/my-app/src/Profile/Profile.js
+++ Compa/my-app/src/Profile/Profile.js
@@ -3,7 +3,7 @@ import "./Profile.css";
 import PhotoGallery from "./PhotoGallery";
 
 const Profile = () => {
-  const data = ["tag1", "tag2", "tag3"]; // Данные для тегов
+  const data = ["tag1", "tag2", "tag3", "teg4"]; // Данные для тегов
 
   return (
     <>
@@ -11,7 +11,10 @@ const Profile = () => {
         <div className="profile-container">
           <img className="profile-image" />
           <div>
-            <h2 className="h2-profile">user name</h2>
+            <div className="block-style">
+              <h2 className="h2-profile">user name</h2>
+              <img src="/Group 7.png" className="img-chats" />
+            </div>
             <div>
               <div className="InfoAboutUser-profile">
                 information about user information about user information about
@@ -20,13 +23,21 @@ const Profile = () => {
             </div>
           </div>
 
-          <ul className="tegList-profile">
-            {data.map((item, index) => (
-              <li key={index}>
-                <div className="teg-profile">{item}</div>
-              </li>
-            ))}
-          </ul>
+          <div className="block-style1">
+            <ul className="tegList-profile">
+              {data.map((item, index) => (
+                <li key={index}>
+                  <div className="teg-profile">{item}</div>
+                </li>
+              ))}
+            </ul>
+            <div>
+              <button className="tegs-plus">
+                <span>+</span>
+              </button>
+            </div>
+          </div>
+
           <div className="MY-event">Мои события</div>
           <div>events events</div>
           <div>
